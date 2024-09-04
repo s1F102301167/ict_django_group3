eec4befae4b55e2a3c83b0221f94c6eaf3413ed3
s1F102301167 <136313317+s1F102301167@users.noreply.github.com>
2024-09-04 14:08:59 +0900

    talk

--- Legal_Support_Mate/views.py
+++ Legal_Support_Mate/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'Legal_Support_Mate/index.html')
+
+def talk(request):
+    return render(request, 'Legal_Support_Mate/talk.html')
