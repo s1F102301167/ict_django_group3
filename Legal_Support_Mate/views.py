ef26ddbfa57393c3b1697276f34dbc3e169aaa7f
s1f10230101 <s1f102301011@iniad.org>
2024-09-04 14:09:34 +0900

    chatのページを追加

--- Legal_Support_Mate/views.py
+++ Legal_Support_Mate/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'Legal_Support_Mate/index.html')
+
+def chat(request):
+    return render(request, 'Legal_Support_Mate/chat.html')
--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Legal_Support_Mate import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Legal_Support_Mate/', views.index)
+    path('Legal_Support_Mate/', include('Legal_Support_Mate.urls')),
 ]
