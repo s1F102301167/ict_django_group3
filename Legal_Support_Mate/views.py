17c895997f0eaec0ae599325969aa4cfceb632af
s1f10230101 <s1f102301011@iniad.org>
2024-09-04 16:09:13 +0900

    キャッシュファイルを管理から削除

--- Legal_Support_Mate/views.py
+++ Legal_Support_Mate/views.py
@@ -7,7 +7,22 @@ def index(request):
 
 def talk(request, category: str):
     print(category)
+    messages = []
+    ans = []
+    if request.method == 'POST':
+        msg = request.POST['message']
+        messages.append(msg)
+        ans.append(gpt_response(msg))
     context = {
-        'category': category
+        'category': category,
+        'messages': messages,
+        "gpt_anser": ans
     }
     return render(request, 'Legal_Support_Mate/talk.html', context)
+
+def gpt_response(question: str) -> str:
+    a = call_gpt(question)
+    return a
+
+def call_gpt(_: str) -> str:
+    return "gpt answer"
